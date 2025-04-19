f4b286650dcd1487454f09fe95cf42461833406e
shaiksaheid <shaikshaheid8@gmail.com>
2025-04-19 11:27:25 +0530

    Update app.py
    Prepare app for Render deployment

--- app.py
+++ app.py
@@ -207,5 +207,6 @@ def previous_data():
     return render_template("previous_data.html", data=processed_data, columns=columns)
 
 
-if __name__ == '__main__':
-    app.run(debug=True)
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
